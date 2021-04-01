980148d7aa25f88a5789aab7a3cc38099c5a6f2c
Samuel Oliveira <samuel.virtuoso@hotmail.com>
2021-04-01 16:27:49 -0300

    Atualização

--- routes.js
+++ routes.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const route = express.Router()
 const homeController = require('./src/controllers/homeController')
-const middlewares = require('./src/middlewares/middlewares')
 
 route.get('/', homeController.HomePage) 
 
